f7be53843d57e46da23e4693607de8ecd94fbde9
dhruv <97798512+dhruv-git-hub@users.noreply.github.com>
2022-08-07 17:21:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,8 @@ import asyncio
 from pyrogram import Client
 
 api_id = 11363673
-api_hash = '9092d33af68c40aeed371f278ffd75ee'
+api_hash = '9092d33af68c40aeed371f278ffd75ee',
+bot_token='5290496073:AAGr8DiKkiyk1LeY81inSUKCZqiWQ0nuwy4'
 
 
 async def main():
