df216770cf71c37179bb94f5a0b18572353f6fd3
dhruv <97798512+dhruv-git-hub@users.noreply.github.com>
2022-08-08 21:03:15 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -33,7 +33,7 @@ def command3(bot,message):
     
 @bot.on_message(filters.command('song'))
 def command3(bot,message):
-    bot.send_audio(message.chat.id,"https://www.youtube.com/watch?v=HxxNFEyDAy0")
+    bot.send_video(message.chat.id,"https://www.youtube.com/watch?v=HxxNFEyDAy0")
     
     
 print("Sucessfully completed an orbit")
